3059e089f9687b5a357587f95504a47b4c6c76eb
sachinhridayraj <49241230+sachinhridayraj@users.noreply.github.com>
2019-04-11 11:59:47 -0700

    Depluralizing product names in search

--- src/PartsUnlimitedWebsite/ProductSearch/StringContainsProductSearch.cs
+++ src/PartsUnlimitedWebsite/ProductSearch/StringContainsProductSearch.cs
@@ -21,7 +21,7 @@ namespace PartsUnlimited.ProductSearch
         {
 			try
 			{
-				var cleanQuery = query;
+				var cleanQuery = Depluralize(query);
 				
 				var q = _context.Products
 					.Where(p => p.Title.ToLower().Contains(cleanQuery));
@@ -33,6 +33,23 @@ namespace PartsUnlimited.ProductSearch
 				return new List<Product>();
 			}
         }
+	
+		public string Depluralize(string query)
+		{
+			if (query.EndsWith("ies"))
+			{
+				query = query.Substring(0, query.Length - 3) + "y";
+			}
+			else if (query.EndsWith("es"))
+			{
+				query = query.Substring(0, query.Length - 1);
+			}
+			else if (query.EndsWith("s"))
+			{
+				query = query.Substring(1, query.Length);
+			}
+			return query.ToLowerInvariant();
+		}
 
 	}
 }
